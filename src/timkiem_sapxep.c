b3768491f464e6d348e4b3ee54e18704e73231ec
A-3Wcode <luthanhtai38@gmail.com>
2026-08-25 14:18:14 +0700

    Add search and sort function declarations
    Added function declarations for search and sort operations.

--- src/timkiem_sapxep.c
+++ src/timkiem_sapxep.c
@@ -2,3 +2,8 @@
 \*
 Các hàm timKiemTheoMa, timKiemTheoTen, timKiemTheoLoai, timKiemTheoKhoangGia, sapXepTheoMa, sapXepTheoTen, sapXepTheoGia.
 *\
+// 4. Tim kiem
+MA* timKiemTheoMa(TD *td, char ma[]);
+TD timKiemTheoTen(TD *td, char ten[]);
+TD timKiemTheoLoai(TD *td, char loai[]);
+TD timKiemTheoKhoangGia(TD *td, double giaMin, double giaMax);
