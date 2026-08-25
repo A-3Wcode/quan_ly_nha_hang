65847144ce4aeb8a4e668d0a2c41e612ba723f4c
A-3Wcode <luthanhtai38@gmail.com>
2026-08-25 15:40:33 +0700

    Update banan.c

--- src/banan.c
+++ src/banan.c
@@ -1,7 +1,52 @@
 #include "../include/nha_hang.h"
 
 // 8. Quan ly Ban an & Thanh toan
-void khoiTaoBan(Ban *ban, int soBan);
-bool goiMon(Ban *ban, MA *mon);
-double tinhTongTien(Ban *ban);
-void thanhToanBan(Ban *ban);
+void khoiTaoBan(Ban *ban, int soBan)
+{
+    ban->SoBan = soBan;
+    ban->TrangThai = false;
+    ban->HoaDon= NULL;
+    ban->TongHoaDon = 0;
+}
+bool goiMon(Ban*ban, MA*mon)
+{
+    if (ban == NULL || mon == NULL)
+        return false;
+    MA *monMoi = taoMonAn(
+        mon->MaMon,
+        mon->Ten,
+        mon->Loai,
+        mon->Gia
+    );
+    if (monMoi == NULL)
+        return false;
+    monMoi->next = ban->HoaDon;
+    ban->HoaDon = monMoi;
+    ban->TrangThai = true;
+    return true;
+}
+double tinhTongTien(Ban *ban)
+{
+    double tong = 0;
+    MA *p = ban->HoaDon;
+    while (p != NULL)
+    {
+    tong += p->Gia;
+    p = p->next;
+    }
+    ban->TongHoaDon = tong;
+    return tong;
+}
+void thanhToanBan(Ban *ban)
+{
+    MA *p=ban->HoaDon;
+    while (p != NULL)
+    {
+    MA *temp = p;
+    p = p->next;
+    free(temp);
+    }
+    ban->HoaDon = NULL;
+    ban->TongHoaDon = 0;
+    ban->TrangThai = false;
+}
