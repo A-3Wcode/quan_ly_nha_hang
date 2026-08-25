49b23f27c62e734b07515c34f8ed7c83dcff99c6
A-3Wcode <luthanhtai38@gmail.com>
2026-08-25 15:40:51 +0700

    Remove unused table management functions
    Removed unused functions related to table management.

--- src/fileio.c
+++ src/fileio.c
@@ -1,5 +1,4 @@
 #include "../include/nha_hang.h"
-#include "../include/nha_hang.h"
 bool ghiFileThucDon(TD*td, char tenFile[])
 {
     FILE*f = fopen(tenFile, "w");
@@ -28,53 +27,4 @@ bool docFileThucDon(TD*td, char tenFile[])
     }
     fclose(f);
     return true;
-}
-void khoiTaoBan(Ban *ban, int soBan)
-{
-    ban->SoBan = soBan;
-    ban->TrangThai = false;
-    ban->HoaDon= NULL;
-    ban->TongHoaDon = 0;
-}
-bool goiMon(Ban*ban, MA*mon)
-{
-    if (ban == NULL || mon == NULL)
-        return false;
-    MA *monMoi = taoMonAn(
-        mon->MaMon,
-        mon->Ten,
-        mon->Loai,
-        mon->Gia
-    );
-    if (monMoi == NULL)
-        return false;
-    monMoi->next = ban->HoaDon;
-    ban->HoaDon = monMoi;
-    ban->TrangThai = true;
-    return true;
-}
-double tinhTongTien(Ban *ban)
-{
-    double tong = 0;
-    MA *p = ban->HoaDon;
-    while (p != NULL)
-    {
-    tong += p->Gia;
-    p = p->next;
-    }
-    ban->TongHoaDon = tong;
-    return tong;
-}
-void thanhToanBan(Ban *ban)
-{
-    MA *p=ban->HoaDon;
-    while (p != NULL)
-    {
-    MA *temp = p;
-    p = p->next;
-    free(temp);
-    }
-    ban->HoaDon = NULL;
-    ban->TongHoaDon = 0;
-    ban->TrangThai = false;
 }
