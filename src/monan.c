3602d426f0edec0d0500fffac8c0aef6f2ff080a
A-3Wcode <luthanhtai38@gmail.com>
2026-08-25 14:13:24 +0700

    Add function declarations for dish management
    Added function declarations for menu management and dish handling.

--- src/monan.c
+++ src/monan.c
@@ -2,3 +2,17 @@
 /*
 Các hàm taoMonAn, khoiTaoThucDon, giaiPhongThucDon, kiemTraTrungMa, themMonAn, nhapDanhSachMonAn, hienThiMonAn, hienThiThucDon.
 */
+// 1. Khoi tao va Quan ly bo nho
+MA* taoMonAn(char ma[], char ten[], char loai[], double gia);
+void khoiTaoThucDon(TD *td);
+void giaiPhongThucDon(TD *td);
+
+// 2. Nhap va Them mon
+bool kiemTraTrungMa(TD *td, char ma[]);
+bool kiemTraHopLeMonAn(char ma[], char ten[], double gia);
+bool themMonAn(TD *td, MA *monMoi);
+void nhapDanhSachMonAn(TD *td);
+
+// 3. Hien thi
+void hienThiMonAn(MA *mon);
+void hienThiThucDon(TD *td);
