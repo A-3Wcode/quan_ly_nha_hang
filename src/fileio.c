4869e20155568c5a7ce90048023d7f8911ea278f
A-3Wcode <luthanhtai38@gmail.com>
2026-08-25 14:48:10 +0700

    Clean up comments in fileio.c
    Removed commented-out documentation for file functions.

--- src/fileio.c
+++ src/fileio.c
@@ -1,7 +1,5 @@
 #include "../include/nha_hang.h"
-\*
-Các hàm ghiFileThucDon, docFileThucDon.
-*\
+
 // 7. Doc va Ghi file text
 bool ghiFileThucDon(TD *td, char tenFile[]);
 bool docFileThucDon(TD *td, char tenFile[]);
