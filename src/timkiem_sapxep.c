ce70149c3728c1c3578c89bc9858020098205c08
A-3Wcode <luthanhtai38@gmail.com>
2026-08-25 14:48:41 +0700

    Clean up comments in timkiem_sapxep.c
    Removed commented-out documentation for search and sort functions.

--- src/timkiem_sapxep.c
+++ src/timkiem_sapxep.c
@@ -1,7 +1,5 @@
 #include "../include/nha_hang.h"
-\*
-Các hàm timKiemTheoMa, timKiemTheoTen, timKiemTheoLoai, timKiemTheoKhoangGia, sapXepTheoMa, sapXepTheoTen, sapXepTheoGia.
-*\
+
 // 4. Tim kiem
 MA* timKiemTheoMa(TD *td, char ma[]);
 TD timKiemTheoTen(TD *td, char ten[]);
