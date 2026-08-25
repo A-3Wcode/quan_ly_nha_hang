5cccab2a3a244009ad9980569b2de4bbd5c0b68b
A-3Wcode <luthanhtai38@gmail.com>
2026-08-25 14:17:37 +0700

    Update banan.c

--- src/banan.c
+++ src/banan.c
@@ -1,1 +1,9 @@
-
+#include "../include/nha_hang.h"
+/*
+Các hàm khoiTaoBan, goiMon, tinhTongTien, thanhToanBan.
+*/
+// 8. Quan ly Ban an & Thanh toan
+void khoiTaoBan(Ban *ban, int soBan);
+bool goiMon(Ban *ban, MA *mon);
+double tinhTongTien(Ban *ban);
+void thanhToanBan(Ban *ban);
