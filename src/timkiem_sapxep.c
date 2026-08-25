f12a6aeadfa3e9730f795c1ad1fb9013b86b17ed
A-3Wcode <luthanhtai38@gmail.com>
2026-08-25 14:19:10 +0700

    Add function declarations for updating and sorting

--- src/timkiem_sapxep.c
+++ src/timkiem_sapxep.c
@@ -7,3 +7,9 @@ MA* timKiemTheoMa(TD *td, char ma[]);
 TD timKiemTheoTen(TD *td, char ten[]);
 TD timKiemTheoLoai(TD *td, char loai[]);
 TD timKiemTheoKhoangGia(TD *td, double giaMin, double giaMax);
+
+// 6. Cap nhat va Sap xep
+bool capNhatMonAn(TD *td, char ma[]);
+void sapXepTheoMa(TD *td, bool tangDan);
+void sapXepTheoTen(TD *td, bool tangDan);
+void sapXepTheoGia(TD *td, bool tangDan);
