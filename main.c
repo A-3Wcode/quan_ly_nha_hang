a940299f5f22e30da912051150bf2100d6977555
A-3Wcode <luthanhtai38@gmail.com>
2026-08-27 10:09:30 +0700

    Update main.c

--- main.c
+++ main.c
@@ -26,16 +26,15 @@ int main() {
         scanf("%d", &luonChon);
 
         switch (luonChon) {
-            case 1:
-                // Goi ham them mon
-                break;
-            case 2:
-                hienThiThucDon(&td);
-                break;
-            case 0:
-                giaiPhongThucDon(&td);
-                printf("Da thoat chuong trinh.\n");
-                break;
+            case 1:{}
+            case 2:{}
+            case 3:{}
+            case 4:{}
+            case 5:{}
+            case 6:{}
+            case 7:{}
+            case 8:{}
+            case 0:{}
             default:
                 printf("Lua chon khong hop le!\n");
         }
