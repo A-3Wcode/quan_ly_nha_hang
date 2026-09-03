f91bc8f2a19af64e78d777213249c4e142bc6211
Quang <lasao4321@gmail.com>
2026-09-03 11:27:30 +0700

    Update fileio.c

--- src/fileio.c
+++ src/fileio.c
@@ -1,30 +1,30 @@
 #include "../include/nha_hang.h"
 /*Thuc hien: Tran Pham Vinh Quang*/
-bool ghiFileThucDon(TD*td, char tenFile[])
+bool ghiFileThucDon(TD* td, char tenFile[])
 {
-    FILE*f = fopen(tenFile, "w");
-    if (f== NULL)
+    FILE* f = fopen(tenFile, "w");
+    if (f == NULL)
         return false;
-    MA*p =td->head;
-    while(p!= NULL)
+    MA* p = td->head;
+    while (p != NULL)
     {
-        fprintf(f,"%s|%s|%s|%.2f\n",p->MaMon, p->Ten, p->Loai, p->Gia);
+        fprintf(f, "%s,%s,%s,%.lf\n", p->MaMon, p->Ten, p->Loai, p->Gia);
         p = p->next;
     }
     fclose(f);
     return true;
 }
-bool docFileThucDon(TD*td, char tenFile[])
+bool docFileThucDon(TD* td, char tenFile[])
 {
-    FILE*f = fopen(tenFile, "r");
-    if (f== NULL)
+    FILE* f = fopen(tenFile, "r");
+    if (f == NULL)
         return false;
-    char ma[50], ten[50], loai[50];
-    float  gia;
-    while (fscanf(f, "%49[^|]|%49[^|]|%49[^|]|%lf\n",ma, ten, loai, &gia)== 4)
+    char ma[15], ten[50], loai[50];
+    double gia;
+        while (fscanf(f, "%14[^,],%49[^,],%49[^,],%lf\n", ma, ten, loai, &gia) == 4)
     {
-    MA*monMoi = taoMonAn(ma, ten, loai, gia);
-    themMonAn(td, monMoi);
+        MA* monMoi = taoMonAn(ma, ten, loai, gia);
+        themMonAn(td, monMoi);
     }
     fclose(f);
     return true;
